85d8fe79953f71ed45d04612ec35bd43fdcdf84e
ytlll <1340876809@qq.com>
2021-09-22 20:19:10 +0800

    redux

---
+++ 10_react_redux/src/pages/about.js
@@ -0,0 +1,46 @@
+import React, { PureComponent } from 'react'
+import store from '../store';
+import { subAction } from '../store/actionCreators';
+
+export default class About extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: store.getState().counter
+    }
+  }
+
+  componentDidMount() {
+    // 订阅store
+    this.unsubscribe = store.subscribe(() => {
+      this.setState({
+        counter: store.getState().counter
+      })
+    })
+  }
+
+  componentWillUnmount() {
+    // 取消订阅
+    this.unsubscribe()
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>当前计数: {this.state.counter}</h2>
+        <button onClick={e => this.decrement()}>-1</button>
+        <button onClick={e => this.subNumber(5)}>-5</button>
+      </div>
+    )
+  }
+
+  decrement() {
+    store.dispatch(subAction(1));
+  }
+
+  subNumber(num) {
+    store.dispatch(subAction(num));
+  } 
+}
